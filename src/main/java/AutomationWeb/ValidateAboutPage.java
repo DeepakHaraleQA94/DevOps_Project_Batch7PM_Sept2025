085c7a0869d4ffc03494ca9adfecfc973996c49e
NilkantiL <loharnilkanti@gmail.com>
2025-09-05 19:21:09 +0530

    Added new statement Hello ABCD

--- src/main/java/AutomationWeb/ValidateAboutPage.java
+++ src/main/java/AutomationWeb/ValidateAboutPage.java
@@ -5,5 +5,6 @@ public class ValidateAboutPage {
 	public static void main(String[] args) {
 		System.out.println("Hello AWS");
 		System.out.println("Hello DevOps");
+		System.out.println("Hello ABCD");
 	}
 }
